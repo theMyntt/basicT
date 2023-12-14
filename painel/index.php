8e307004335c12a4bf2aa7a363b56e7612d84fd0
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2023-12-14 00:04:11 -0300

    feat: ver usuários já cadastrados

--- painel/index.php
+++ painel/index.php
@@ -43,12 +43,13 @@
 					<button class="btn blue darken-2" onclick="location.href = './newSale/'">Nova venda</button>
 					<button class="btn blue darken-2" onclick="location.href = './hisSale/'">Histórico de Vendas</button>
 					<button class="btn blue darken-2" onclick="location.href = './hisLogin/'">Histórico de Acessos</button>
-					<button class="btn blue darken-2" onclick="location.href = '../'">Sair</button>
+					<button class="btn red" onclick="location.href = '../'">Sair</button>
 				</div>
 			</div>
 			<div class="card-panel">
 				<h5>Configurações do sistema</h5>
 				<div class="row">
+					<button class="btn blue darken-2" onclick="location.href='./hisUser/'">Ver usuários já existentes</button>
 					<button class="btn blue darken-2" onclick="location.href='./cadUser/'">Cadastrar usuário</button>
 					<button class="btn blue darken-2" onclick="location.href='./rmUser/'">Deletar usuário existente</button>
 				</div>
